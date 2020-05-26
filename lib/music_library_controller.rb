f4bc2ed4e69ddaacab03ccfb9b7547d0645cd79f
michael thomas <michael.thomas2014@gmail.com>
2020-05-26 23:01:35 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -85,8 +85,8 @@ class MusicLibraryController
     input = gets.chomp
     input = input.to_i
     if input >= 1 && input <= list_songs.length 
-      binding.pry
       song = list_songs[(input-1)]
+      binding.pry
       puts "Playing #{song.name} by #{song.artist.name}"
     end
       
